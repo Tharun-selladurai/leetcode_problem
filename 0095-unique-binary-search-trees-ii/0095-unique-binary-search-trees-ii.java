9dd5fb2cf443f1d7ebb565973f06f1a47a50f884
tharunselladurai <tharunselladurai123@gmail.com>
2025-01-29 07:54:46 +0530

    Time: 1 ms (98.11%), Space: 44.4 MB (95.03%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) { this.val = val; }
+}
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return generateTreesHelper(1, n);
+    }
+
+    private List<TreeNode> generateTreesHelper(int start, int end) {
+        List<TreeNode> trees = new ArrayList<>();
+        if (start > end) {
+            trees.add(null);
+            return trees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = generateTreesHelper(start, i - 1);
+            List<TreeNode> rightSubtrees = generateTreesHelper(i + 1, end);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    trees.add(root);
+                }
+            }
+        }
+        return trees;
+    }
+}
